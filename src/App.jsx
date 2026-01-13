fcc9723549c7abe6513d92cb61f9f390584698fa
sistech-dev <sisaytadesse@loyalistcollege.com>
2026-01-13 15:03:29 -0600

    Assignment 1: added my name

--- src/App.jsx
+++ src/App.jsx
@@ -22,7 +22,7 @@ function App() {
           count is {count}
         </button>
         <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
+          "Hello Sisay Tadesse !""
         </p>
       </div>
       <p className="read-the-docs">
